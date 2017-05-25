e173db0ffcadce75bd5c9230c9ba5fdf61430ab9
Esha More <Esha More>
2017-05-24 17:45:45 -0700

    Organized

---
+++ scripts/BuildLineChart.R
@@ -0,0 +1,5 @@
+# buildtimeline.R (ESHA)
+# For tweets: bar chart of tweets over time (answers: when do people tweet the most?)
+# For weather: line graph of temperature (scale so it will sit on same graph as tweets)
+# If both are checked, render them on top of one another
+# Add correlation
